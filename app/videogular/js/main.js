fbf69a7fce007551abf403a6b610d6001a908d69
tedyuen <tedyuen.goo@gmail.com>
2017-02-28 21:14:56 +0800

    Add own mp4.

--- app/videogular/js/main.js
+++ app/videogular/js/main.js
@@ -13,19 +13,20 @@ angular.module('myApp',
       this.config = {
         preload: "none",
         sources: [
-          {src: $sce.trustAsResourceUrl("http://static.videogular.com/assets/videos/videogular.mp4"), type: "video/mp4"},
-          {src: $sce.trustAsResourceUrl("http://static.videogular.com/assets/videos/videogular.webm"), type: "video/webm"},
-          {src: $sce.trustAsResourceUrl("http://static.videogular.com/assets/videos/videogular.ogg"), type: "video/ogg"}
+          {src: $sce.trustAsResourceUrl("video/yyzhiru.mp4"), type: "video/mp4"},
+          // {src: $sce.trustAsResourceUrl("http://static.videogular.com/assets/videos/videogular.mp4"), type: "video/mp4"},
+          // {src: $sce.trustAsResourceUrl("http://static.videogular.com/assets/videos/videogular.webm"), type: "video/webm"},
+          // {src: $sce.trustAsResourceUrl("http://static.videogular.com/assets/videos/videogular.ogg"), type: "video/ogg"}
         ],
-        tracks: [
-          {
-            src: "http://www.videogular.com/assets/subs/pale-blue-dot.vtt",
-            kind: "subtitles",
-            srclang: "en",
-            label: "English",
-            default: ""
-          }
-        ],
+        // tracks: [
+        //   {
+        //     src: "http://www.videogular.com/assets/subs/pale-blue-dot.vtt",
+        //     kind: "subtitles",
+        //     srclang: "en",
+        //     label: "English",
+        //     default: ""
+        //   }
+        // ],
         theme: {
           url: "http://www.videogular.com/styles/themes/default/latest/videogular.css"
         }
